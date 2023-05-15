186e2cda56b2b42a78711cecef7c19f6f2043aaa
LupoVerde01 <federico-fabi@libero.it>
2023-05-15 20:58:12 +0200

    Aggiunta anagrafica provenienza (Migration)

---
+++ MascheraCatasto/Anagrafiche/Provenienza.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace MascheraCatasto.Anagrafiche
+{
+    public enum Provenienza
+    {
+        [Description("Firenze")]
+        Firenze = 1,
+
+        [Description("Residenza a Firenze")]
+        Residenza_Firenze = 2,
+
+        [Description("Italia (non Firenze)")]
+        Italia = 3,
+
+        [Description("Germania")]
+        Germania = 4,
+
+        [Description("Altro (No Germania o Italia)")]
+        Altro = 5,
+
+        [Description("Residenza in Italia (non Firenze)")]
+        Residenza_Italia = 6,
+
+        [Description("Residenza in Germania")]
+        Residenza_Germania = 7,
+
+        [Description("Residenza No Germania o Italia")]
+        Residenza_Altro = 8,
+
+    }
+}
